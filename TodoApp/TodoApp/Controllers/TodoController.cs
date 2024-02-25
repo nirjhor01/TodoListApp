6cbd6789649a06cdce0973b3c89ef2665e3a9a21
Nirjhor <tanveer.ahmed@technonext.com>
2024-02-25 17:58:45 +0600

    committed

---
+++ TodoApp/TodoApp/Controllers/TodoController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+using TodoApp.Models;
+
+namespace TodoApp.Controllers
+{
+    public class TodoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        
+        public ActionResult Save(Layer layer)
+        {
+            SqlConnection conn = new SqlConnection("Data Source=(localdb)\\localDB; Initial Catalog = Todoo; Integrated Security = true;");
+            conn.Open();
+            string que = "INSERT INTO info(id,TaskName,Description,status)VALUES('" + layer.id + "','" + layer.TaskName + "','" + layer.Description + "','" + layer.Status + "')";
+            SqlCommand cmd = new SqlCommand(que, conn);
+           // try
+           // {
+               
+            int cnt = cmd.ExecuteNonQuery();
+            string s = "";
+            if (cnt > 0)
+            {
+                s = "successful";
+            }
+            else
+            {
+                s = "Unsuccessful";
+            }
+            ViewBag.msg = s;
+            return View();
+               
+            //}
+            //catch (Exception ex)
+            //{
+             //   return "Exception occured";
+            //}
+          
+        }
+        public ActionResult SaveAll()
+        {
+            return View();
+        }
+        public ActionResult Rdex()
+        {
+            Layer layer = new Layer();
+            layer.TaskName = "Nirjhor";
+            layer.Description = "abcd";
+            layer.Status = "ok";
+            ViewBag.msg = layer;
+
+            return View();
+        }
+        public ActionResult GetAll()
+        {
+            return View();
+        }
+       
+    }
+}
---
+++ TodoApp/TodoApp/Models/Layer.cs
@@ -0,0 +1,10 @@
+﻿namespace TodoApp.Models
+{
+    public class Layer
+    {
+        public string id {  get; set; }
+        public string TaskName { get; set; }
+        public string Description { get; set; }
+        public string Status { get; set; }
+    }
+}
